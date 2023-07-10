115d782d0c03f3a81682384ee7b3a18db06e41a1
Harsh <harshpachani@gmail.com>
2023-07-10 22:27:53 +0530

    Express.js explaination

--- 2.indexForExpress.js
+++ 2.indexForExpress.js
@@ -1,8 +1,17 @@
 import express from "express";
+
+//path module is used to specify the absolute location of the file.
+import path from "path";
+
 const port = 5000;
 
-const server = express();
+const app = express();
 
-server.listen(port, () => {
+app.get("/", (req, res) => {
+    const absolutePath = path.basename();
+    console.log(path.join(absolutePath, "/index.html"));
+});
+
+app.listen(port, () => {
     console.log(`Server is working on http://localhost:${port}`);
 });
