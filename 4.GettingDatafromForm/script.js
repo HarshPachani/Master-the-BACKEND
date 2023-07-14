23d27687a4e7d936e234db7c078766322515c7ed
Harsh <harshpachani@gmail.com>
2023-07-15 00:57:00 +0530

    Initial commit

--- 4.GettingDatafromForm/script.js
+++ 4.GettingDatafromForm/script.js
@@ -3,17 +3,43 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = 3000;
+const port = 5000;
+
+const users = [];
 
 //for setting up view engine
 app.set("view engine", "ejs");
 
+//Using Middlewares.
 //for showing static(public) folder data.
 app.use(express.static(path.join(path.resolve(), "public")));
 
-app.get("", (req, res) => {
+//for accessing the data submitted by user with form.
+app.use(express.urlencoded({ encoded: true }));
+
+app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/success", (req, res) => {
+    res.render("success");
+});
+
+app.get("/users", (req, res) => {
+    res.json({
+        users,
+    });
 })
+
+app.post("/contact", (req, res) => {
+    // console.log(req.body);
+    // console.log(req.body.name);
+    // console.log(req.body.email);
+    users.push({username: req.body.name, email: req.body.email});
+
+    res.redirect("/success");
+});
+
 app.listen(port, () => {
     console.log(`The Server is working on port http://localhost:${port}`);
 });
