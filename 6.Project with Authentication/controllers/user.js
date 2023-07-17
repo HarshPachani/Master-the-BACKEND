3519d09171c8a8534ea3df9cd85c489b6c1eabcf
Harsh <harshpachani@gmail.com>
2023-07-17 12:37:24 +0530

    Added a config.env configurations

--- 6.Project with Authentication/controllers/user.js
+++ 6.Project with Authentication/controllers/user.js
@@ -46,7 +46,14 @@ export const getMyProfile = (req, res) => {
 };
 
 export const logoutUser = (req, res) => {
-    res.status(200).cookie("token", "", {expires: new Date(Date.now())}).json({
+    res
+        .status(200)
+        .cookie("token", "", {
+            expires: new Date(Date.now()),
+            sameSite: process.env.NODE_ENV === "Development"?"lax":"none",
+            secure: process.env.NODE_ENV === "Development"? false : true,
+        })
+        .json({
         success: true,
         user: req.user,
     });
