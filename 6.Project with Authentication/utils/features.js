155750d44e6360976faadbd4c94f802a53352ee6
Harsh <harshpachani@gmail.com>
2023-07-17 12:37:47 +0530

    Added a config.env configurations

--- 6.Project with Authentication/utils/features.js
+++ 6.Project with Authentication/utils/features.js
@@ -8,6 +8,8 @@ export const sendCookie = (user, res, message, statusCode=200) => {
         cookie("token", token, {
             httpOnly: true,        
             maxAge: 15 * 60 * 1000, //expiry time = 15 minutes. 15 * minutes * second(1) 
+            sameSite: process.env.NODE_ENV === "Development"?"lax":"none",
+            secure: process.env.NODE_ENV === "Development"? false : true,
         })
         .json({
             success: true,
